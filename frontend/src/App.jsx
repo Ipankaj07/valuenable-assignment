32d020eb53b1bfccb937c83d447b181759f803f2
Pankaj <Praj4936@gmail.com>
2023-01-14 13:30:30 +0530

    Changed Header position

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AllRoutes from "./routes/AllRoutes";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <div className="h-full bg-gradient-to-b from-gray-900 to-slate-800 flex flex-col text-white">
+        <Header />
         <AllRoutes />
       </div>
     </div>
--- frontend/src/routes/AllRoutes.jsx
+++ frontend/src/routes/AllRoutes.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import Header from "../components/Header";
 import Login from "../components/User/Login";
 import Register from "../components/User/Register";
 import Policy from "../components/Policy";
@@ -9,7 +8,6 @@ import Policy from "../components/Policy";
 const AllRoutes = () => {
   return (
     <>
-      <Header />
       <Routes>
         <Route path="/" element={<Policy />} />
         <Route path="/login" element={<Login />} />
